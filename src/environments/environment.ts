d13ccc4709dfdfc8d196e1b029d8fd073f88dd41
Wayne Gakuo <waynejotham@gmail.com>
2025-10-09 07:55:59 +0300

    feat(environment): add Firebase configuration for development and production builds

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,12 @@
 export const environment = {
   production: true,
-  firebaseConfig: {}
+  firebaseConfig: {
+    apiKey: "AIzaSyBivTCXySJQh3r-EAtxjwLQkYEViRcmqzk",
+    authDomain: "bytewise-shop.firebaseapp.com",
+    projectId: "bytewise-shop",
+    storageBucket: "bytewise-shop.firebasestorage.app",
+    messagingSenderId: "70492809529",
+    appId: "1:70492809529:web:d03a0ac3a823355fa220e8",
+    measurementId: "G-4THZCND3FP"
+  }
 };
